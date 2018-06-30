63b9ed81969cec6338e8d773acdae2b27a80377d
Denys Levchenko <denis.levtchenko@gmail.com>
2018-06-30 15:40:19 +0200

    update solution

--- _solution/polls/management/commands/import_questions.py
+++ _solution/polls/management/commands/import_questions.py
@@ -10,34 +10,6 @@ from polls.models import Question, Choice, QuestionInfo
 class Command(BaseCommand):
     help = 'Import Questions from file'
 
-    def _handle(self, *args, **options):
-        """
-        {"choices": [["Choice 0 0", 9], ["Choice 0 1", 7],
-                     ["Choice 0 2", 10], ["Choice 0 3", 11]],
-                     "author": "Author 0", "value": 30, "question_text": "Question N 0"}
-        :param args:
-        :param options:
-        :return:
-        """
-        file_name = 'questions_data.json'
-        with open(file_name, 'r') as outfile:
-            questions = json.load(outfile)
-
-        start = time()
-        print(len(questions))
-        n = 0
-
-        for question in questions:
-            q = Question.objects.create(question_text=question['question_text'])
-            QuestionInfo.objects.create(question=q, author=question['author'], value=question['value'])
-            [Choice.objects.create(choice_text=text, votes=votes, question=q)
-             for text, votes in question['choices']]
-            n += 1
-            if n % 50 == 0:
-                print(n)
-
-        self.stdout.write(self.style.SUCCESS('Successfully imported. Time "%s"' % time() - start))
-
     def handle(self, *args, **options):
         """
         {"choices": [["Choice 0 0", 9], ["Choice 0 1", 7],
@@ -47,55 +19,62 @@ class Command(BaseCommand):
         :param options:
         :return:
         """
-        file_name = 'questions_data.json'
-        with open(file_name, 'r') as outfile:
+        with open('questions_data.json') as outfile:
             questions = json.load(outfile)
-
+        n = 0
         start = time()
         print(len(questions))
-        n = 0
-        questions_to_insert = []
+
+        questions_db = []
         questions_dict = {}
-        n_id = 1
         for question in questions:
-            questions_to_insert.append(
+            questions_db.append(
                 Question(
-                    question_text=question['question_text'],
-                    id=n_id
+                    question_text=question['question_text']
                 )
             )
-            questions_dict[n_id] = question
-            n_id += 1
-
-        Question.objects.bulk_create(questions_to_insert, batch_size=1000)
+            questions_dict[question['question_text']] = question
+        Question.objects.bulk_create(
+            questions_db,
+            batch_size=10000
+        )
 
         questions = Question.objects.all()
-
-        for record in queryset_iterator(questions):
+        for question_chunk in queryset_iterator(questions):
             choices = []
-            question_info = []
-            for question in record:
-                question_data = questions_dict[question.id]
+            infos = []
+            for question in question_chunk:
+                q = questions_dict.get(question.question_text)
                 choices.extend(
-                    [Choice(choice_text=text, votes=votes, question=question)
-                     for text, votes in question_data['choices']]
+                    Choice(
+                        choice_text=text,
+                        votes=votes,
+                        question=question
+                    ) for text, votes in q['choices']
                 )
-                question_info.append(
-                    QuestionInfo(author=question_data['author'], question=question, value=question_data['value']))
-
-            Choice.objects.bulk_create(choices, 1000)
-            QuestionInfo.objects.bulk_create(question_info)
-            n += 1000
+                infos.append(
+                    QuestionInfo(
+                        author=q['author'],
+                        value=q['value'],
+                        question=question
+                    )
+                )
+            QuestionInfo.objects.bulk_create(infos)
+            Choice.objects.bulk_create(choices, batch_size=5000)
+            # if n % 50 == 0:
+            #     print(n)
+            n += 5000
             print(n)
 
         self.stdout.write(self.style.SUCCESS('Successfully imported. Time "%s"' % (time() - start)))
 
 
-def queryset_iterator(queryset, chunksize=1000):
-    pk = 0
-    last_pk = queryset.order_by('-pk')[0].pk
-    queryset = queryset.order_by('pk')
-    while pk < last_pk:
-        chunk = list(queryset.filter(pk__gt=pk)[:chunksize])
-        pk = chunk[-1].pk
+
+def queryset_iterator(queryset, chunksize=5000):
+    id = 0
+    last_id = queryset.order_by('-id')[0].id
+    queryset = queryset.order_by('id')
+    while id < last_id:
+        chunk = list(queryset.filter(id__gt=id)[:chunksize])
+        id = chunk[-1].id
         yield chunk
